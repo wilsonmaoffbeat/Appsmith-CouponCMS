0c37e086f1cb94d48c4750435810f585b3e699c7
wilson.ma <wilson.ma@offbeattech.com>
2024-11-25 08:30:20 +0000

    -fixed ViewCouponDetail discount display -fixed and completed CreateCoupon page (discount)

--- pages/CreateCoupon/jsobjects/js_on_discount_type_change/js_on_discount_type_change.js
+++ pages/CreateCoupon/jsobjects/js_on_discount_type_change/js_on_discount_type_change.js
@@ -1,6 +1,7 @@
 export default {
 	onDiscountTypeChange()
 	{
+		this.iniCouponType();
 		if(select_discount_type.selectedOptionValue === 'FIXED_AMOUNT')
 		{
 			input_discount_percent.setValue("");
@@ -24,6 +25,20 @@ export default {
 			input_discount_amount.setDisabled(true);
 			input_discount_percent.setDisabled(true);
 			input_max_discount_amount.setDisabled(true);
+		}
+	},
+	iniCouponType()
+	{
+		storeValue("couponTypes", 
+		[
+			{"name": "PRODUCT","code": "PRODUCT"},
+			{"name": "DELIVERY","code": "DELIVERY"}
+		]);
+		if(select_discount_type.selectedOptionValue === 'PERCENTAGE') //remove PRODUCT if it is a percentage coupon
+		{
+			select_coupon_type.setSelectedOption("DELIVERY");
+			const types = appsmith.store.couponTypes || [];
+			storeValue("couponTypes", types.filter(item => !(item.name === "PRODUCT" && item.code === "PRODUCT")));
 		}
 	}
 }
