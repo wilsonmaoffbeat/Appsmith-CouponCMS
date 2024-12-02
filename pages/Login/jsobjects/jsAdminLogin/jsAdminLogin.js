cb82e87f3ee155c9da575688e1e6e2e62c51b0ea
wilson.ma <wilson.ma@offbeattech.com>
2024-12-02 02:36:18 +0000

    -implemented UAC for login

--- pages/Login/jsobjects/jsAdminLogin/jsAdminLogin.js
+++ pages/Login/jsobjects/jsAdminLogin/jsAdminLogin.js
@@ -21,35 +21,68 @@ export default {
 		const payload = [client, keyName, email, password, timestamp].join("");
 		const signature = CryptoJS.HmacSHA256(payload, keyValue).toString();
 
-		const data = await fetch(`https://admin-api.${domainMapping.get(env)}/internal-identities/auth/password`, {
-			method: "POST",
-			body: JSON.stringify({
-				client,
-				email,
-				password,
-				credential: keyName,
-				timestamp,
-				signature
-			}),
-			headers: {
-				"Content-Type": "application/json"
-			}
-		}).then((it) => {
-			return it.ok ? it.json() : Promise.reject(it.statusText)
-		})
+		
+		if(this.checkAuth())
+		{
+			const data = await fetch(`https://admin-api.${domainMapping.get(env)}/internal-identities/auth/password`, {
+				method: "POST",
+				body: JSON.stringify({
+					client,
+					email,
+					password,
+					credential: keyName,
+					timestamp,
+					signature
+				}),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			}).then((it) => {
+				return it.ok ? it.json() : Promise.reject(it.statusText)
+			})
+		
+			storeValue('everutsAdminToken', data.token, true);
+			storeValue('everutsRefreshToken', data.refreshToken, true);
+			storeValue('everutsAdminApiRoot', `https://admin-api.${domainMapping.get(env)}`, true);
 
-		storeValue('everutsAdminToken', data.token, true);
-		storeValue('everutsRefreshToken', data.refreshToken, true);
-		storeValue('everutsAdminApiRoot', `https://admin-api.${domainMapping.get(env)}`, true);
-		
-		//login
-		storeValue("testAdminToken", data.token, true);
-		storeValue("testRefreshToken", data.refreshToken, true);
-		storeValue("testAdminApiRoot", `https://admin-api.${domainMapping.get(env)}`, true);
-		storeValue("testApiRoot", `https://api.${domainMapping.get(env)}`, true);
-		//storeValue("testAdminApiRoot", "https://admin-api.everutm.link", true);
-		//storeValue("testApiRoot", "https://api.everutm.link", true);
-		
-		navigateTo('CouponList');
+			//login
+			storeValue("testAdminToken", data.token, true);
+			storeValue("testRefreshToken", data.refreshToken, true);
+			storeValue("testAdminApiRoot", `https://admin-api.${domainMapping.get(env)}`, true);
+			storeValue("testApiRoot", `https://api.${domainMapping.get(env)}`, true);
+			//storeValue("testAdminApiRoot", "https://admin-api.everutm.link", true);
+			//storeValue("testApiRoot", "https://api.everutm.link", true);
+
+			navigateTo('CouponList');
+		}
+		else
+		{
+			showAlert('Wrong account info/Your account is not authorized!', 'error');
+		}
+	},
+	checkAuth() //check if user is wthin the list
+	{
+		const emailList = this.genEmailArray();
+		const canAuth = emailList.includes(uiLoginEmailInput.text.toLowerCase());
+		if(emailList.length > 0 && uiLoginEmailInput.text !== '' && uiLoginEmailInput.text !== null && canAuth === true)
+			return true;
+		else
+			return false;
+	},
+	genEmailArray() //return auth list
+	{
+		return [
+			'dion.lam@offbeattech.com',
+			'wilson.ma@offbeattech.com',
+			'katrina.choy@offbeattech.com',
+			'bertha.wong@offbeattech.com',
+			'qubie.cheng@offbeattech.com',
+			'carole.ma@offbeattech.com',
+			'oscar.kwok@offbeattech.com',
+			'jamie.ong@offbeattech.com',
+			'joyce.lin@offbeattech.com',
+			'rex.lam@offbeattech.com',
+			'yolanda.wong@offbeattech.com'
+		]
 	}
 }
